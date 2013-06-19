0c6e5616a1a2ec59cef20d512da178891a179995
Seth Goings <seth.goings@readytalk.com>
2013-06-19 13:56:44 -0600

    Add start to WrapperCreatorPluginTest.groovy

---
+++ wrapper-creator/src/test/groovy/plugins/ReadyTalkWrapperTest.groovy
@@ -0,0 +1,11 @@
+package tasks
+
+import spock.lang.*
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.api.Task
+
+class WrapperCreatorPluginTest extends Specification {
+
+}
