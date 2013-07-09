a6b14712ed079fecfbe0bd1eb1f4ca60692e7fb3
Doug Borg <douglas.borg@readytalk.com>
2013-07-09 15:36:48 -0600

    Fix duplicate files / bad filter operation.

--- wrapper-creator/src/main/groovy/tasks/PackageWrapper.groovy
+++ wrapper-creator/src/main/groovy/tasks/PackageWrapper.groovy
@@ -40,29 +40,24 @@ class PackageWrapper extends Zip {
 
   void setJvmOpts(String options) {
     jvmOpts = options
+
     if(options) {
-
-      into('') {
-
-        from(stockWrapper) {
-          include  '**/bin/gradle'
-          filter { line ->
-            if (line =~ "DEFAULT_JVM_OPTS=") {
-              return "DEFAULT_JVM_OPTS=\"${options}\""
-            } else {
-              return line
-            }
+      filesMatching('**/bin/gradle') {
+        filter { line ->
+          if (line =~ "DEFAULT_JVM_OPTS=") {
+            return "DEFAULT_JVM_OPTS=\"${options}\""
+          } else {
+            return line
           }
         }
+      }
 
-        from(stockWrapper) {
-          include  '**/bin/gradle.bat'
-          filter { line ->
-            if (line =~ "set DEFAULT_JVM_OPTS=") {
-              return "set DEFAULT_JVM_OPTS=${options}"
-            } else {
-              return line
-            }
+      filesMatching('**/bin/gradle.bat') {
+        filter { line ->
+          if (line =~ "set DEFAULT_JVM_OPTS=") {
+            return "set DEFAULT_JVM_OPTS=${options}"
+          } else {
+            return line
           }
         }
       }
